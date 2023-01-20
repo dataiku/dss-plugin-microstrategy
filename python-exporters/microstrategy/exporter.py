1e203fb0dacd401de0a62fc40cb3fbdd99c984fa
Alexandre Bourret <alexandre.bourret@dataiku.com>
2023-01-20 14:52:34 +0100

    Variable renaming + PEP8

--- python-exporters/microstrategy/exporter.py
+++ python-exporters/microstrategy/exporter.py
@@ -1,4 +1,3 @@
-import os
 import logging
 import requests
 import pandas as pd
@@ -8,7 +7,7 @@ from mstrio import microstrategy
 
 import dataiku
 from dataiku.exporter import Exporter
-from dataiku.exporter import SchemaHelper
+
 
 logging.basicConfig(level=logging.INFO, format='dss-plugin-microstrategy %(levelname)s - %(message)s')
 logger = logging.getLogger()
@@ -34,40 +33,42 @@ class CustomExporter(Exporter):
         """
         self.row_buffer = []
         self.buffer_size = 5000
-
+        logger.info("Starting MicroStrategy exporter v1.1.1-beta.2")
         # Plugin settings
         self.base_url = plugin_config.get("base_url", None)
         self.project_name = config["microstrategy_project"].get("project_name", None)
-        self.project_id = "" # the project id, obtained through a later request
+        self.project_id = ""  # the project id, obtained through a later request
         self.dataset_name = str(config.get("dataset_name", None)).replace(" (created by Dataiku DSS)", "") + " (created by Dataiku DSS)"
-        self.dataset_id = "" # the dataset id, obtained at creation or on update
+        self.dataset_id = ""  # the dataset id, obtained at creation or on update
         self.table_name = "dss_data"
         self.username = config["microstrategy_api"].get("username", None)
-        self.password = config["microstrategy_api"].get("password",'')
+        self.password = config["microstrategy_api"].get("password", '')
 
         if not (self.username and self.base_url):
             logger.error('Connection params: {}'.format(
-                {'username:': self.username,
-                'password:': '#' * len(self.password),
-                'base_url:': self.base_url})
+                {
+                    'username:': self.username,
+                    'password:': '#' * len(self.password),
+                    'base_url:': self.base_url
+                })
             )
             raise ValueError("username and base_url must be filled")
 
-        self.conn = microstrategy.Connection(base_url=self.base_url, username=self.username, password=self.password, project_name=self.project_name)
-
+        self.connection = microstrategy.Connection(base_url=self.base_url, username=self.username, password=self.password, project_name=self.project_name)
 
     def open(self, schema):
         self.dss_columns_types = get_dss_columns_types(schema)
         (self.schema, dtypes, parse_dates_columns) = dataiku.Dataset.get_dataframe_schema_st(schema["columns"])
 
         # Prevent problems when reading int
-        # If we don't use it too, the initialization of the cube does not have the same dtypes as the read data (in write_row) and we get mismatchs when updating
+        # If we don't use it too, the initialization of the cube does not have the same dtypes as the read data (in write_row)
+        # and we get mismatchs when updating
         if dtypes is not None:
             new_dtypes = {}
-            for (k, v) in dtypes.items():
-                if v == np.int64 or v == np.int32:
-                    v = np.float64
-                new_dtypes[k] = v
+            for (key, value) in dtypes.items():
+                if value == np.int64 or value == np.int32:
+                    value = np.float64
+                new_dtypes[key] = value
             dtypes = new_dtypes
 
         if parse_dates_columns:
@@ -78,36 +79,46 @@ class CustomExporter(Exporter):
         logger.info("Will create mstr dataset with dtypes = %s" % dtypes)
         self.dataframe = pd.DataFrame({k: pd.Series(dtype=dtypes[k]) for k in dtypes})
 
-
         logger.info('Opening connection to mstr')
         try:
-            self.conn.connect()
-        except Exception as e:
+            self.connection.connect()
+        except Exception as error_message:
             logger.exception("Connection to MicroStrategy failed.")
-            raise e
+            raise error_message
 
         # Get a project list, search for our project in the list, get the project ID for future API calls.
-        r = requests.get(url = self.base_url+"/projects", headers = {"X-MSTR-AuthToken": self.conn.auth_token}, cookies= self.conn.cookies)
-        r.raise_for_status()
-        projects_list = r.json()
-
+        response = requests.get(url=self.base_url+"/projects", headers={"X-MSTR-AuthToken": self.connection.auth_token}, cookies=self.connection.cookies)
+        response.raise_for_status()
+        try:
+            projects_list = response.json()
+        except Exception as error_message:
+            raise error_message
         for project in projects_list:
             if project["name"] == self.project_name:
                 self.project_id = project["id"]
                 break
 
+        if not self.project_id:
+            raise ValueError("Project '{}' could not be found on this server.".format(self.project_name))
+
         # Search for objects of type 3 (datasets/cubes) with the right name
-        r = requests.get(url = self.base_url+"/searches/results", headers = {"X-MSTR-AuthToken": self.conn.auth_token, "X-MSTR-ProjectID": self.project_id}, cookies = self.conn.cookies, params = {"name": self.dataset_name, "type": 3})
-        r.raise_for_status()
-        search_results = r.json()
+        response = requests.get(
+            url=self.base_url+"/searches/results",
+            headers={"X-MSTR-AuthToken": self.connection.auth_token, "X-MSTR-ProjectID": self.project_id},
+            cookies=self.connection.cookies, params={"name": self.dataset_name, "type": 3}
+        )
+        response.raise_for_status()
+        search_results = response.json()
 
         # No result, create a new dataset
         if search_results["totalItems"] == 0:
             try:
-                self.dataset_id, newTableId = self.conn.create_dataset(data_frame=self.dataframe, dataset_name=self.dataset_name, table_name=self.table_name)
-            except Exception as e:
-                logger.exception("Dataset creation issue:")
-                raise e
+                self.dataset_id, newTableId = self.connection.create_dataset(
+                    data_frame=self.dataframe, dataset_name=self.dataset_name, table_name=self.table_name
+                )
+            except Exception as error_message:
+                logger.exception("Dataset creation issue: {}".format(error_message))
+                raise error_message
         # Found exactly 1 result, fetch the dataset ID
         elif search_results["totalItems"] == 1:
             self.dataset_id = search_results["result"][0]["id"]
@@ -116,16 +127,15 @@ class CustomExporter(Exporter):
             raise RuntimeError('Found two datasets named {} on your MicroStrategy instance.'.format(self.dataset_name))
 
         # Replace data (drop existing) by sending the empty dataframe, with correct schema
-        self.conn.update_dataset(data_frame=self.dataframe, dataset_id=self.dataset_id, table_name=self.table_name, update_policy='replace')
-
+        self.connection.update_dataset(data_frame=self.dataframe, dataset_id=self.dataset_id, table_name=self.table_name, update_policy='replace')
 
     def write_row(self, row):
         row_dict = {}
         for (column_name, cell_value, dtype) in zip(self.schema, row, self.dss_columns_types):
-            if (not cell_value or (type(cell_value)==float and np.isnan(cell_value))) and dtype=='string':
+            if (not cell_value or (type(cell_value) == float and np.isnan(cell_value))) and dtype == 'string':
                 cell_value = ""
-            if (type(cell_value)==float and np.isnan(cell_value)) and dtype=='boolean':
-                logger.error("Boolean column {} contains an empty cell".format(column_name))
+            if (type(cell_value) == float and np.isnan(cell_value)) and dtype == 'boolean':
+                logger.error("Boolean column '{}' contains an empty cell".format(column_name))
                 raise ValueError(
                     "There is an empty cell in the boolean column '{}'. ".format(column_name)
                     + "Boolean columns can only contain true/false values."
@@ -138,19 +148,17 @@ class CustomExporter(Exporter):
             self.flush_data(self.row_buffer)
             self.row_buffer = []
 
-
     def close(self):
         logger.info("Sending {} final rows to MicroStrategy.".format(len(self.row_buffer)))
         self.flush_data(self.row_buffer)
-
         logger.info("Logging out.")
-        r = requests.get(url = self.base_url+"/auth/logout", headers = {"X-MSTR-AuthToken": self.conn.auth_token}, cookies= self.conn.cookies)
-        logger.info("Logout returned status {}".format(r.status_code))
+        response = requests.get(url=self.base_url+"/auth/logout", headers={"X-MSTR-AuthToken": self.connection.auth_token}, cookies=self.connection.cookies)
+        logger.info("Logout returned status {}".format(response.status_code))
 
     def flush_data(self, rows):
         self.dataframe = pd.DataFrame(rows)
         try:
-            self.conn.update_dataset(data_frame=self.dataframe, dataset_id=self.dataset_id, table_name=self.table_name, update_policy='add')
+            self.connection.update_dataset(data_frame=self.dataframe, dataset_id=self.dataset_id, table_name=self.table_name, update_policy='add')
         except Exception as error_message:
             logger.exception("Dataset update issue: {}".format(error_message))
             raise error_message
