5656ed2387c9fc58b53b980ddd831f78409bb41f
Alexandre Bourret <alexandre.bourret@dataiku.com>
2023-02-08 14:53:03 +0100

    Removing beta tag

--- python-exporters/microstrategy/exporter.py
+++ python-exporters/microstrategy/exporter.py
@@ -37,7 +37,7 @@ class CustomExporter(Exporter):
         """
         self.row_buffer = []
         self.buffer_size = 5000
-        logger.info("Starting MicroStrategy exporter v1.1.1-beta.4")
+        logger.info("Starting MicroStrategy exporter v1.1.1")
         # Plugin settings
         self.base_url = plugin_config.get("base_url", None)
         self.project_name = config["microstrategy_project"].get("project_name", None)
